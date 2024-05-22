3d884b0ce3c7ac8856b4f3737da08fcf0ba00ef2
cornchiplick <ilsunkiss@gmail.com>
2024-05-22 22:22:21 +0900

    Chore: firebase 초기화 - npm install firebase@10.1.0 - 콘칩구글계정 사용함

---
+++ src/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDrJ4IDAkleg6wFMUeC7ZYzkKeqM9MbB7s",
+  authDomain: "nwitter-reloaded-62113.firebaseapp.com",
+  projectId: "nwitter-reloaded-62113",
+  storageBucket: "nwitter-reloaded-62113.appspot.com",
+  messagingSenderId: "440594447348",
+  appId: "1:440594447348:web:d22ce8dcf9e2da61c657a1",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
